f0fbc9f17a16b22548d4afc531737a160cec36b8
Kai Reed <khreed@college.harvard.edu>
2025-04-08 02:33:16 -0400

    Improve Jest setup for GitHub Actions environment

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -1,13 +1,25 @@
 // Polyfill for browser globals used by cheerio/undici
-global.ReadableStream = class ReadableStream {};
-global.FormData = class FormData {};
-global.Blob = class Blob {};
-global.File = class File {};
-global.Headers = class Headers {
+global.ReadableStream = global.ReadableStream || class ReadableStream {};
+global.FormData = global.FormData || class FormData {};
+global.Blob = global.Blob || class Blob {};
+global.File = global.File || class File {};
+global.Headers = global.Headers || class Headers {
   constructor() {}
   append() {}
   set() {}
   get() {}
 };
-global.Request = class Request {};
-global.Response = class Response {};
+global.Request = global.Request || class Request {};
+global.Response = global.Response || class Response {};
+
+// Make sure these are defined for the nock dependency
+global.fetch = global.fetch || (() => Promise.resolve());
+global.WebSocket = global.WebSocket || class WebSocket {};
+
+// Silence console.error during tests
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  // Don't print "Error fetching URL" messages during tests
+  if (args[0] === 'Error fetching URL:') return;
+  originalConsoleError(...args);
+};
